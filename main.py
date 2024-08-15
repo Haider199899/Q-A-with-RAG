650ac24d2cd0c6ec55d841c95ad7c917c2bb8706
tr-haider <mhaidershahab@technologyrivers.com>
2024-08-15 13:22:02 +0500

    implementation of rag components

--- main.py
+++ main.py
@@ -1,50 +1,67 @@
-import nltk
-import re
-from datasets import load_dataset
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('punkt_tab')
+import bs4
+from langchain import hub
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.chat_models import ChatOllama
+from langchain_chroma import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-stop_words = set(stopwords.words('english'))
-stemmer = PorterStemmer()
-def get_dataset_info(ds):
-    train_ds = ds['train']
-    test_ds = ds['test']
-    train_ds_size = len(ds['train'])
-    test_ds_size = len(ds['test'])
-    ds_header = list(ds['train'][0].keys())
-    return train_ds, test_ds, train_ds_size, test_ds_size, ds_header
-def preprocess(text):
-    text = text.lower()
-    # Remove special characters and numbers
-    text = re.sub(r'\W+', ' ', text)
-    words = word_tokenize(text)
-    # Remove stopwords and stem the words
-    words = [word for word in words if word not in stop_words]
-    # Join the words back into a string
-    return ' '.join(words)
-def pre_process_dataset(ds, ds_header):
-    preprocessed_data = {}
-    for header in ds_header:
-        preprocessed_data[header] = []
-    for entry in ds:
-        context, question, answer = entry['context'], entry['question'], entry['answer']
-        if context != None and question != None and answer != None:
-           preprocessed_data['context'].append(preprocess(context))
-           preprocessed_data['question'].append(preprocess(question))
-           preprocessed_data['answer'].append(preprocess(answer))
-        break
-    print(preprocessed_data['context'][0])
-    print(preprocessed_data['question'][0])
-    print(preprocessed_data['answer'][0])
+#  Access the "nomic-embed-text" embedding model
+embedding_model = OllamaEmbeddings(model="nomic-embed-text", show_progress=True)
+# Access the "mistral" model
+llm = ChatOllama(model="mistral")
+prompt = hub.pull("rlm/rag-prompt")
+# Indexing
 
+def load(doc_url):
+    # Load, chunk and index the contents of the blog.
+    loader = WebBaseLoader(
+        web_paths=(doc_url,),
+        bs_kwargs=dict(
+            parse_only=bs4.SoupStrainer(
+                class_=("post-content", "post-title", "post-header")
+            )
+        ),
+    )
+    docs = loader.load()
+    return docs
 
+def split(docs):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+    return splits
 
+def store(splits):
+    vectorstore = Chroma.from_documents(documents=splits, embedding=embedding_model,
+    collection_name="local-ai")
+    return vectorstore
 
+# Retrieval & Generation
 
-ds = load_dataset("neural-bridge/rag-dataset-12000")
-train_ds, test_ds, train_ds_size, test_ds_size, ds_header = get_dataset_info(ds)
-pre_process_dataset(train_ds,ds_header)
+def retrieve(vectorstore):
+    return vectorstore.as_retriever()
+
+def generate(retriever, query):
+
+    rag_chain = (
+            {"context": retriever | format_docs, "question": RunnablePassthrough()}
+            | prompt
+            | llm
+            | StrOutputParser()
+    )
+    return rag_chain.invoke(query)
+
+# Formatting Document
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+if __name__ == '__main__':
+    docs = load("https://lilianweng.github.io/posts/2023-06-23-agent/")
+    splits = split(docs)
+    vectorstore = store(splits)
+    retriever = retrieve(vectorstore)
+    response= generate(retriever,"What is the document all about?")
+    print(response)
+
